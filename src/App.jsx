18f4d97ee537766000a62a5f689ee0c77148141d
Camila Jullyane <camilaj.silva0@gmail.com>
2024-06-18 21:19:43 -0300

    config eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,11 +6,11 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+ 
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
+         
         </a>
       </div>
       <h1>Vite + React</h1>
